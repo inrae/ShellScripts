a3607ddccce20aae75bf7545c5f8a95ed77e135b
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-09-23 12:57:13 +0200

    Add files via upload

--- transfert_ssh.sh
+++ transfert_ssh.sh
@@ -1,107 +1,157 @@
 #!/bin/bash
 # Author : CABOS Matthieu
-# Date : 26/08/2020
+# Date : 23/07/2020
 
 function usage(){
-	printf"
+	echo"
 Please to use the script with the correct number of arguments :
-./transfert.sh <mode> <user> <source folder> <destination folder> <filename/foldername>
+./transfert.sh <mode> <user> <source folder> <destination folder> <filename/foldername> <IP>
 
 Where :
 * mode is the way to transfert between
-	0 mean upload file to the ssh specified destination folder
-	1 mean download file since the ssh specified source folder
-	2 mean upload folder to the ssh specified destination folder
-	3 mean download folder since the ssh specified source folder
+	1 mean upload file to the ssh specified destination folder
+	2 mean download file since the ssh specified source folder
+	3 mean upload folder to the ssh specified destination folder
+	4 mean download folder since the ssh specified source folder
 * user is your standard user name on the ssh plateform
-* source folder is the name of the source repertory
-* destination foolder is the name of the destination repertory
-* ip is the adress of the ssh server
+* Source folder is the name of the source repertory
+* Destination folder is the name of the destination repertory
 * filename is the exact files name to transfert or the folder name to transfert
-	"
+* IP adress"
 }
 
 param=$*
 ind=1
 files=""
-for i in $param                                       # getting the parameters into variables
+index=0
+home_flag_src=0
+home_flag_dst=0
+IP=0
+if [ $# -eq 0 ] || [ $# -ne 6 ] || [ $1 -gt 4 ] || [ $1 -le 0 ]
+	then
+		usage
+		exit
+fi
+
+if [ "$1" = "--help" ]
+	then
+		usage
+		exit
+fi
+
+for i in $param
 	do
 		if [ $ind -eq 1 ]
 		then
 			mode=$i
 		elif [ $ind -eq 2 ]
-			then
-				user=$i 
+		then
+			user=$i 
 		elif [ $ind -eq 3 ]
+		then
+			if [ $i = ~ ]
 			then
-				source=$i
+				home_flag_src=1
+			fi
+			source=$i
 		elif [ $ind -eq 4 ]
+		then
+			if [ $i = ~ ]
 			then
-				dest=$i 
+				home_flag_dst=1
+			fi
+			dest=$i
 		elif [ $ind -eq 5 ]
-			then
-				ip=$i
+		then
+			files="$i"
 		else
-			files="$files"" $i"
+			IP="$i"
 		fi
 		ind=$((ind+1))
 	done
-index=0
 
 
-if [ $# -eq 0 ]                                       # Help procedure
+source_way=$source
+dest_way=$dest
+
+
+# Xchange source and dest when Mode
+if [ $mode -eq 1 -o $mode -eq 3 ] 
+then
+# 		tmp=$source
+# 		source=$dest
+# 		dest=$tmp
+	if [ $home_flag_src -eq 1 ]
 	then
-		usage
-		exit
+		source_way="/home/$USER"
+	fi
+	if [ $home_flag_dst -eq 1 ]
+	then
+		dest_way="/users/$user"
+	fi
+else
+	if [ $home_flag_src -eq 1 ]
+	then
+		source_way="/users/$user"
+	fi
+	if [ $home_flag_dst -eq 1 ]
+	then
+		dest_way="/home/$USER"
+	fi
 fi
 
-if [ "$1" = "--help" ]                                # Help procedure
+# source_way="."
+# dir=`find | grep "[^/.][A-Za-z0-9_]*$"`   # Getting the source repertory absolute way
+# for i in $dir
+# 	do
+# 		name="${i##*/}"
+# 		if [ "$name" = "$source" ]
+# 			then
+# 				source_way=$i
+# 		fi
+# 	done
+
+# dir=`ssh $user@194.57.114.202 'find'`      # Getting the Folders Architecture
+
+# dest_way="."
+# for i in $dir
+# 	do
+# 		name="${i##*/}"
+# 		if [ "$name" = "$dest" ]
+# 			then
+# 				dest_way=$i
+# 		fi
+# 	done
+
+source_way=$source_way"/"
+dest_way=$dest_way"/"
+
+if [ $mode -eq 1 ]
 	then
-		usage
-		exit
-fi
-
-
-if [ $mode -eq 1 -o $mode -eq 3 ]                     # Xchange source and dest when Download Mode is on
-	then
-		tmp=$source
-		source=$dest
-		dest=$tmp
-fi
-
-source_way="."
-dir=`find | grep "[^/.][A-Za-z0-9_]*$"`               # Getting the source repertory absolute way
-for i in $dir
-	do
-		name="${i##*/}"
-		if [ "$name" = "$source" ]
-			then
-				source_way=$i
+		if [ `echo $dest_way | grep "home/" ` != "" ] 2> /dev/null
+		then
+			dest_way=`echo $dest_way | sed -e "s|/home/$USER|/users/$user|g"`
 		fi
-	done
-
-dir=`ssh $user@$ip 'find'`                            # Getting the Folders Architecture
-
-dest_way="."
-for i in $dir
-	do
-		name="${i##*/}"
-		if [ "$name" = "$dest" ]
-			then
-				dest_way=$i
+		scp $source_way$files $user@$IP:$dest_way
+elif [ $mode -eq 2 ]
+	then
+		if [ `echo $source_way | grep "home/"` != "" ] 2> /dev/null
+		then
+			source_way=`echo $source_way | sed -e "s|/home/$USER/|/users/$user/|g"`
 		fi
-	done
-
-if [ $mode -eq 0 ]                                    # Single File Transfert File Protocol
-	then
-		scp $files $user@$ip:$dest_way 
-elif [ $mode -eq 1 ]
-	then
-		scp $user@$ip:./$files $source_way
-elif [ $mode -eq 2 ]                                  # Folder Transfert Protocol
-	then
-		scp -r $files $user@$ip:$dest_way
+		scp $user@$IP:$source_way$files $dest_way
 elif [ $mode -eq 3 ]
 	then
-		scp -r $user@$ip:./$files $source_way
+		if [ `echo $dest_way | grep "home/"` != "" ] 2> /dev/null
+		then
+			dest_way=`echo $dest_way | sed -e "s|/home/$USER|/users/$user|g"`
+		fi
+		scp -r $source_way$files $user@$IP:$dest_way
+elif [ $mode -eq 4 ]
+	then
+		if [ `echo $source_way | grep "home/"` != "" ] 2> /dev/null
+		then
+			source_way=`echo $source_way | sed -e "s|/home/$USER/|/users/$user/|g"`
+		fi
+		scp -r $user@$IP:$source_way$files $dest_way
 fi
