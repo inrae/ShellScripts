c72d4a0171e21c389a7f0e8186ac7a7ef4d1d9c3
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-09-25 10:32:48 +0200

    Add files via upload

---
+++ cpu_count.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Author : CABOS Matthieu
+# Date : 03/09/2020
+
+# Pour récupérer le resultat dans une variable integer (depuis le bash)
+# Nb_CPU=`./cpu_count.sh mcabos 194.57.114.202 | cut -d ":" -f2`
+
+function usage(){
+	printf"
+Usage : ./cpu_count <user name> <ip adress>
+"
+}
+
+function error(){
+	printf"
+An error has occured, please to report at matthieu.cabos@tse-fr.eu.
+	"
+}
+
+if [ $# -ne 2 ]
+then
+	usage
+	exit
+fi
+
+id_user=$1
+ip_user=$2
+
+rezz=`ssh $id_user@$ip_user 'cat /proc/cpuinfo | tail -26 | head -1 | cut -d ":" -f2'` || error
+((rezz+=1))
+echo "Le nombre de processeurs accessibles est :""$rezz"
